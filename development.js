43903c592d6bf916888a3e14a621575839c39643
Taiga KIYOKAWA <taiga.k127@gmail.com>
2019-06-15 00:26:43 +0900

    import HtmlWebpackPlugin and add plugins

--- development.js
+++ development.js
@@ -1,4 +1,5 @@
 import path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
 
 const src = path.resolve(__dirname, 'src')
 const dist = path.resolve(__dirname, 'dist')
@@ -26,5 +27,10 @@ export default {
     extensions: ['.js', '.jsx']
   },
 
-  plugins: []
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: src + '/index.html',
+      filename: 'index.html'
+    })
+  ]
 }

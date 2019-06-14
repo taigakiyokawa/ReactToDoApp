2b1cde917a176202789e2d4a111ba7ec3f96511f
Taiga KIYOKAWA <taiga.k127@gmail.com>
2019-06-15 00:46:27 +0900

    index.jsx: display input characters in real-time

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,22 @@ import React from 'react';
 import { render } from 'react-dom';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { massage: 'something' }
+  }
+
+  onChange(e) {
+    this.setState( {message: e.target.value} )
+  }
+  
   render () {
-    return <h1>Hello React!</h1>
+    return (
+      <div>
+        <input type="text" onChange = { this.onChange.bind(this) } />
+        <p>{ this.state.message }</p>
+      </div>
+    )
   }
 }
 
